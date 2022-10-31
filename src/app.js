8420daaaebf9e98dde89a1a05a81f99105d9439e
Joonas Varis <joonas.varis@valu.fi>
2022-10-31 23:20:38 +0200

    Add winter orbs

--- src/app.js
+++ src/app.js
@@ -22,7 +22,88 @@ window.onload = () => {
         setTimer();
         if(luhankaanAikaaMs < 1000){
             clearInterval(id);
+            switchToOrbs();
             return;
         }
     }, 1000);
+
+    function switchToOrbs(){
+        const title = document.getElementById("title");
+        const timer = document.getElementById("timer");
+        const nav = document.getElementById("nav");
+
+        title.remove();
+        timer.remove();
+        nav.remove();
+        
+        const canvas = document.createElement("canvas");
+
+        const wrapper = document.getElementById("wrap");
+        wrapper.appendChild(canvas);
+        const canvasContext = canvas.getContext("2d");
+
+        const cw = document.body.clientWidth;
+        const ch = document.body.clientHeight;
+        
+        canvas.width = cw;
+        canvas.height = ch;
+        
+        const img1 = new Image(233,310);
+        const img2 = new Image(233,310);
+        const img3 = new Image(233,310);
+        const img4 = new Image(233,310);
+        const img5 = new Image(233,310);
+        const img6 = new Image(233,310);
+        const img7 = new Image(233,310);
+        const img8 = new Image(233,310);
+        
+        const images = [
+            img1,
+            img2,
+            img3,
+            img4,
+            img5,
+            img6,
+            img7,
+            img8
+        ]
+        
+        for(const img of images){
+            img.addEventListener("load",loader);
+        }
+        
+        let loaded = false;
+        function loader(){
+            if(!allLoaded){
+                return;
+            }
+            if(loaded){
+                return;
+            }
+            loaded = true;
+        
+            canvasContext.drawImage(images[getRandom(8)], 0, 0, this.width, this.height, getRandom(cw-116), getRandom(ch-155), this.width, this.height);
+            
+            setInterval(() => {
+                canvasContext.drawImage(images[getRandom(8)], 0, 0, this.width, this.height, getRandom(cw-116), getRandom(ch-155), this.width, this.height);
+            },1500)
+        }
+        
+        function getRandom(ceiling){
+            return Math.floor(Math.random()*ceiling);
+        }
+        
+        function allLoaded(){
+            return img1.complete && img2.complete && img3.complete && img4.complete && img5.complete && img6.complete && img7.complete && img8.complete
+        }
+        
+        img1.src = new URL('../assets/winterorb1.jpeg', import.meta.url);
+        img2.src = new URL('../assets/winterorb2.jpeg', import.meta.url);
+        img3.src = new URL('../assets/winterorb3.jpeg', import.meta.url);
+        img4.src = new URL('../assets/winterorb4.jpeg', import.meta.url);
+        img5.src = new URL('../assets/winterorb5.jpeg', import.meta.url);
+        img6.src = new URL('../assets/winterorb6.jpeg', import.meta.url);
+        img7.src = new URL('../assets/winterorb7.jpeg', import.meta.url);
+        img8.src = new URL('../assets/winterorb8.jpeg', import.meta.url);
+    }
 }
